a337bd877d67b4c35ae3717fc41579b15ef0b1e7
sderohan <rohanshinde7212@gmail.com>
2022-04-03 22:16:24 +0530

    add JWTManager implementation

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
--- service/jwt_manager.go
+++ service/jwt_manager.go
@@ -1,1 +1,64 @@
 package service
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type JWTManager struct {
+	secretKey     string
+	tokenDuration time.Duration
+}
+
+type UserClaims struct {
+	jwt.StandardClaims
+	Username string `json:"username"`
+	Role     string `json:"role"`
+}
+
+//NewJWTManager returns a new JWT manager
+func NewJWTManager(secretKey string, duration time.Duration) *JWTManager {
+	return &JWTManager{secretKey: secretKey, tokenDuration: duration}
+}
+
+// GenerateToken generates and signs a new token for a user
+func (manager *JWTManager) GenerateToken(user *User) (string, error) {
+	claims := UserClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(manager.tokenDuration).Unix(),
+		},
+		Username: user.Username,
+		Role:     user.Role,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	return token.SignedString([]byte(manager.secretKey))
+}
+
+// VerifyToken verifies the access token string and returns a user claim if token is valid
+func (manager *JWTManager) VerifyToken(accessToken string) (*UserClaims, error) {
+	token, err := jwt.ParseWithClaims(
+		accessToken,
+		&UserClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			_, ok := token.Method.(*jwt.SigningMethodHMAC)
+			if !ok {
+				return nil, fmt.Errorf("unexpected token signing method")
+			}
+
+			return []byte(manager.secretKey), nil
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("invalid token: %w", err)
+	}
+
+	claims, ok := token.Claims.(*UserClaims)
+	if !ok {
+		return nil, fmt.Errorf("invalid token claims")
+	}
+
+	return claims, nil
+}
